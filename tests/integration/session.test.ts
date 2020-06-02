0c226157a06fb83697443d4a25cbaf5b99d614f3
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-06-02 04:55:38 -0400

    Add simple session test -> superoak with bug Change dockerfile to only run start script Change denonson DENO_ENV var to test by default and override to produ... ction inside start

--- tests/integration/session.test.ts
+++ tests/integration/session.test.ts
@@ -1,11 +1,23 @@
-import { assert, equal, assertNotEquals } from 'https://deno.land/std/testing/asserts.ts';
 import User from '../../src/app/models/User.ts';
+import app from '../../src/app.ts';
+import { superoak, SuperDeno } from 'https://deno.land/x/superoak/mod.ts';
+import { describe, it } from 'https://deno.land/x/superoak/test/utils.ts';
 
-const user = new User();
-user.name = 'test';
-user.email = 'test@test.com'
-user.password_hash = '12345'
+describe('Authentication', () => {
+  it('should authenticate with valid credentials', async (done: any) => {
+    const user = new User();
+    user.name = 'test';
+    user.email = 'test@test.com';
+    user.password_hash = '12345';
+    await user.save();
 
-Deno.test('Authentication', () => {
-    equal(5, 5);
+    const response: SuperDeno = await superoak(app);
+    response
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: user.password_hash,
+      })
+      .expect(200, done);
+  });
 });
